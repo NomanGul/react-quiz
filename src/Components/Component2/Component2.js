a2bf8b9d6ea7cd03dd5c0df2d85ff13202b80075
Noman Gul <nomangul2001@gmail.com>
2018-09-09 16:46:25 +0500

    Adding Demo Code

---
+++ src/Components/Component2/Component2.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Component2 = ({}) => {
+  return <div />;
+};
+
+export default Component2;
---
+++ src/Components/Component3/Component3.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Component3 = ({}) => {
+  return <div />;
+};
+
+export default Component3;
